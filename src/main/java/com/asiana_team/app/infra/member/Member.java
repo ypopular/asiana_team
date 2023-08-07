69877739f77927ded4d113164f094092a78b4459
dbsdlsrl25@naver.com <dbsdlsrl25@naver.com>
2023-08-07 15:21:20 +0900

    d

---
+++ src/main/java/com/asiana_team/app/infra/member/Member.java
@@ -0,0 +1,70 @@
+package com.asiana_team.app.infra.member;
+
+public class Member {
+	
+	public String seq;
+	public String name;
+	public String en_name;
+	public Integer gender;
+	public String id;
+	public String password;
+	public String tel;
+	public String email_id;
+	public String email_type;
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEn_name() {
+		return en_name;
+	}
+	public void setEn_name(String en_name) {
+		this.en_name = en_name;
+	}
+	public Integer getGender() {
+		return gender;
+	}
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail_id() {
+		return email_id;
+	}
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+	public String getEmail_type() {
+		return email_type;
+	}
+	public void setEmail_type(String email_type) {
+		this.email_type = email_type;
+	}
+	
+	
+}
